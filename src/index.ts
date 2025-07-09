b7f8b8f2359f35927dd7c7d61a0ea9406226571f
BouzelatTarek <106556135+BouzelatTarek@users.noreply.github.com>
2025-07-09 13:00:26 +0200

    update index

--- src/index.ts
+++ src/index.ts
@@ -32,7 +32,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 // config.yaml
-app.use("/config", configRoute);
+//app.use("/config", configRoute);
 
 // Route d'administration
 //app.use("/admin", adminRoute);
